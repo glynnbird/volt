9b972bf0c651cdbbfccd92e4d777b3dc26bbffb1
Glynn Bird <glynn.bird@gmail.com>
2015-11-07 11:42:09 +0000

    added sync

--- js/volt.js
+++ js/volt.js
@@ -2,6 +2,7 @@
 // render the main page
 var showMainPage = function() {
   $('#addformpanel').addClass("hidden");
+  $('#cloudpanel').addClass("hidden");
   $('#status').html("");
   $('#status').removeClass("hidden");
   
@@ -33,9 +34,10 @@ var showMainPage = function() {
 
 
 
-
+// when the page has loaded
 $( document ).ready(function() {
   
+  // show the main pages
   showMainPage();
   
   // when the add button is pressed
@@ -43,6 +45,7 @@ $( document ).ready(function() {
     
     // remove the main page and show the add form
     $('#addformpanel').removeClass("hidden");
+    $('#cloudpanel').addClass("hidden");
     $('#status').addClass("hidden");
     
     // pre-populate the form
@@ -52,6 +55,18 @@ $( document ).ready(function() {
     getCurrentTabUrl(function(err, url) {
       $('#url').val(url);
     });
+  });
+  
+  // when the cloud button is pressed
+  $("#cloudbutton").bind("click", function() {
+    
+    // remove the main page and show the add form
+    $('#cloudpanel').removeClass("hidden");
+    $('#addformpanel').addClass("hidden");
+    $('#status').addClass("hidden");
+    
+    // clear the form
+    $('#cloudurl').val("");
   });
   
   // when the add form submit button is pressed
@@ -75,6 +90,16 @@ $( document ).ready(function() {
     });
   });
   
+  // when the cloud form submit button is pressed
+  $('#cloudform').bind("submit", function(event) {
+    event.preventDefault();
+    
+    var url = $('#cloudurl').val();
+    vaultdb.replicate.to(url)
+     .on("change", function(info) { console.log("change",info)})
+     .on("complete", function(info){ console.log("complete",info)})
+     .on("error", function(err) { console.log("error",err)});
+  });
   
 });
 
