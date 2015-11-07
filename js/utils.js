a31637b275a967d502e64835ac1469f2196636c0
Glynn Bird <glynn.bird@gmail.com>
2015-11-07 10:55:32 +0000

    added add form

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,4 @@
+// extract a domain name from a URL
 var extractDomainName = function(url) {
   var domain = url;
   domain = domain.replace(/https?:\/\//,"");
@@ -5,23 +6,9 @@ var extractDomainName = function(url) {
   return domain;  
 };
 
-var copyToClipboard = function(selector) {
-  // Select the email link anchor text  
-  var el = document.querySelector(selector);  
-  var range = document.createRange();  
-  range.selectNode(el);  
-  window.getSelection().addRange(range);  
-
-  try {  
-    // Now that we've selected the anchor text, execute the copy command  
-    var successful = document.execCommand('copy');  
-    var msg = successful ? 'successful' : 'unsuccessful';  
-    console.log('Copy email command was ' + msg);  
-  } catch(err) {  
-    console.log('Oops, unable to copy');  
-  }  
-
-  // Remove the selections - NOTE: Should use
-  // removeRange(range) when it is supported  
-  window.getSelection().removeAllRanges();  
+// get the currently seleted Chrome tab
+var getCurrentTabUrl = function(callback) {
+  chrome.tabs.getSelected(null,function(tab) {
+    callback(null, tab.url);
+  });
 };
--- js/volt.js
+++ js/volt.js
@@ -1,7 +1,21 @@
-chrome.tabs.getSelected(null,function(tab) {
-    var domain = extractDomainName(tab.url);
+
+// render the main page
+var showMainPage = function() {
+  $('#addformpanel').addClass("hidden");
+  $('#status').html("");
+  $('#status').removeClass("hidden");
+  
+  // calculate the current tab in view
+  getCurrentTabUrl(function(err, url) {
+    if(err || !url) return;
     
+    // extract the domain name from the url
+    var domain = extractDomainName(url);
+  
+    // find all vault entries that match this domain name
     vaultFilter(domain, "monkey",  function(err, data) {
+      
+      // render as a table of passwords
       var html = '<table class="table">';
       html += '<tr><th>Site</th><th>Username</th><th>Password</th></tr>\n';
       for(var i in data) {
@@ -9,8 +23,59 @@ chrome.tabs.getSelected(null,function(tab) {
       }
       html += "</table>";
       $('#status').html(html);
+      
+      // enable clipboard integration
       new Clipboard('.clippy');
-      console.log(err,data);
     });
+  
+  });
+}
 
+
+
+
+$( document ).ready(function() {
+  
+  showMainPage();
+  
+  // when the add button is pressed
+  $("#addbutton").bind("click", function() {
+    
+    // remove the main page and show the add form
+    $('#addformpanel').removeClass("hidden");
+    $('#status').addClass("hidden");
+    
+    // pre-populate the form
+    $('#url').val("");
+    $('#username').val("");
+    $('#password').val("");
+    getCurrentTabUrl(function(err, url) {
+      $('#url').val(url);
+    });
+  });
+  
+  // when the add form submit button is pressed
+  $('#addform').bind("submit", function(event) {
+    event.preventDefault();
+    
+    // create a document to add to the database
+    var doc = {
+      url: $('#url').val(),
+      domain: "",
+      username: $('#username').val(),
+      password: $('#password').val(),
+    }
+    doc.domain = extractDomainName(doc.url);
+    
+    // write it
+    vaultWrite(doc,'monkey', function(err,data) {
+      
+      // re-render the main page
+      showMainPage();
+    });
+  });
+  
+  
 });
+
+
